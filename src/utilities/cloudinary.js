7337458c6054a7c360fce0de8996f85439e0fb2d
SAISARIKA <devisettysaisarika@gmail.com>
2020-12-11 19:21:29 +0530

    cloudinary details

--- src/utilities/cloudinary.js
+++ src/utilities/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require("cloudinary").v2;
 
 
 cloudinary.config({
-  cloud_name: 'dp2yty5vo',
-  api_key: '424567826579411',
-  api_secret: 'iY2bgOs3VxqBQURvBMgPCvual7k',
+  cloud_name: 'jisst',
+  api_key: '434253341977259',
+  api_secret: 'lxq42oPr31z1QxVAaOT2CX3EevM',
 });
 
 module.exports = cloudinary;
